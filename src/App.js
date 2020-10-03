58e5dc76f7b18f80fa8c1ecc0a78281deda394f2
Sam Schoenberg <sschoenberg234@gmail.com>
2020-10-03 14:20:24 -0600

    added travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This is the new test site <br />
+         And it looks pretty awesome
         </p>
         <a
           className="App-link"
